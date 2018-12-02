6ce6b00b6ec7f3ad2baccf8699b8b12cd31089ea
Robin Astle <robin@astle.be>
2018-12-02 22:01:02 +0100

    Added a basic version of an event table

--- app/routes.py
+++ app/routes.py
@@ -12,13 +12,13 @@ def index():
             'timestamp': '2018-11-27 16:55:34.123456',
             'type': 'carEntered',
             'description': 'Car started',
-            'additionalData': '51.20308577,4.49374067'
+            'additional_data': '51.20308577,4.49374067'
         },
         {
             'timestamp': '2018-11-27 17:07:27.123406',
             'type': 'carLeft',
             'description': 'Car stopped',
-            'additionalData': '51.50308577,4.43374067'
+            'additional_data': '51.50308577,4.43374067'
         }
     ]
     return render_template('index.html', title='Home', user=user, events=events)
