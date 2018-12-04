fcc7008604d4eac7f607a06d221ea445329a7f36
robin.astle <robin@astle.be>
2018-12-04 11:25:04 +0100

    Reformated some files

--- pydripa.py
+++ pydripa.py
@@ -1,6 +1,7 @@
 from app import app
-from app.models import User, Event
+from app.models import User, LocationEvent
+
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Event': Event}
+    return {'db': db, 'User': User, 'Event': LocationEvent}
