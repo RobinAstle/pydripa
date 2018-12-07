2f280bb1040d7858755fcbc7f59ab304a5321ba5
robin.astle <robin@astle.be>
2018-12-07 16:21:58 +0100

    Added the user login code.

--- app/models.py
+++ app/models.py
@@ -1,14 +1,15 @@
 from datetime import datetime
-from app import db
+from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    events = db.relationship('Event', backref='user', lazy='dynamic')
+    locationEvents = db.relationship('LocationEvent', backref='user', lazy='dynamic')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -29,3 +30,8 @@ class LocationEvent(db.Model):
 
     def __repr__(self):
         return '<Event {}>'.format(self.description)
+
+
+@login.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
